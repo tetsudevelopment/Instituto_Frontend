f57fc1e3c4f4c0f466473df5ab51df865e8eb3ad
tetsudevelopment <bresmacawork@gmail.com>
2022-08-02 15:44:33 -0500

    Proyecto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
 
